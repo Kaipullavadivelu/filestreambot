ecdcac761532128051c34a6951144bbdd9bba100
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-12-18 23:24:31 +0530

    Update stream.py

--- Code_X_Mania/bot/plugins/stream.py
+++ Code_X_Mania/bot/plugins/stream.py
@@ -106,8 +106,8 @@ async def private_receive_handler(c: Client, m: Message):
             parse_mode="HTML", 
             quote=True,
             disable_web_page_preview=True,
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=wurl), #Stream Link
-                                                InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ📥', url=durl)]]) #Download Link
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🖥STREAM", url=stream_link), #Stream Link
+                                                InlineKeyboardButton('Dᴏᴡɴʟᴏᴀᴅ📥', url=online_link)]]) #Download Link
         )
     except FloodWait as e:
         print(f"Sleeping for {str(e.x)}s")
