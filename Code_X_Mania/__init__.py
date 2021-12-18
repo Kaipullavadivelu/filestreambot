e6cc75e85f44c06a8b6ea507e7c58ef2c8deec35
Kaipullavadivelu <86786455+Kaipullavadivelu@users.noreply.github.com>
2021-12-18 23:17:11 +0530

    Update __init__.py

--- Code_X_Mania/__init__.py
+++ Code_X_Mania/__init__.py
@@ -1,4 +1,25 @@
-# (c) Code-x-Mania
-
+import logging
+import os
 import time
+import random
+import string
+import subprocess
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv('config.env')
+
+def getConfig(name: str):
+    return os.environ[name]
+
 StartTime = time.time()
+
+try:
+    SHORTENER = getConfig('SHORTENER')
+    SHORTENER_API = getConfig('SHORTENER_API')
+    if len(SHORTENER) == 0 or len(SHORTENER_API) == 0:
+        raise KeyError
+except KeyError:
+    SHORTENER = None
+    SHORTENER_API = None
